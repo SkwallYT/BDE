ffeef461cce446c8a6ca247ecce5f28841894f09
SkwallYT <96206807+SkwallYT@users.noreply.github.com>
2024-10-10 17:28:31 +0200

    Update checkVersion.py
    - Finalisation de l'algo de mise a jour et de reparation des fichiers

--- BDE_Budget/checkVersion.py
+++ BDE_Budget/checkVersion.py
@@ -6,6 +6,12 @@
 
 import requests, shutil, os
 
+# Liens vers les fichiers git
+version_url = 'https://raw.githubusercontent.com/SkwallYT/BDE/refs/heads/main/BDE_Budget/Version'
+changelog_url = 'https://raw.githubusercontent.com/SkwallYT/BDE/refs/heads/main/BDE_Budget/FileChangelog'
+getBudgetUI_url = 'https://raw.githubusercontent.com/SkwallYT/BDE/refs/heads/main/BDE_Budget/getBudgetUI.py'
+main_url = 'https://raw.githubusercontent.com/SkwallYT/BDE/refs/heads/main/BDE_Budget/main.py'
+
 def check_file():
     """
     Permet de verifier l'integrite des fichiers de l'application
@@ -29,18 +35,64 @@ def download_file(list_files):
     :return: bool True si tous les fichiers sont good, sinon False
     """
     for elt in list_files:
-        if elt == 'main.py':
-            url =
+
+        try:
+            if elt == 'main.py':
+                reponse = requests.get(main_url)
+                with open(elt, mode='wb') as f:
+                    f.write(reponse.content)
+                    f.close()
+
+            if elt == 'getBudgetUI.py':
+                reponse = requests.get(getBudgetUI_url)
+                with open(elt, mode='wb') as f:
+                    f.write(reponse.content)
+                    f.close()
+
+            if elt == 'Version':
+                reponse = requests.get(version_url)
+                with open(elt, mode='wb') as f:
+                    f.write(reponse.content)
+                    f.close()
+
+            if elt == 'FileChangelog':
+                reponse = requests.get(changelog_url)
+                with open(elt, mode='wb') as f:
+                    f.write(reponse.content)
+                    f.close()
+
+        except Exception as e:
+            print(e)
+            return False
+
+    return True
 
 def version_control():
-    pass
+
+    with open('Version', 'r') as f:
+        version = f.read().strip()
+
+    if version != requests.get(version_url).content:
+        os.remove('FileChangelog')
+        download_file(['FileChangelog'])
+        with open('FileChangelog', 'r') as f:
+            a = f.readlines()
+            f.close()
+
+        file_list = []
+
+        for elt in a:
+            file_list.append(elt[:-1])
+
+        for elt2 in file_list:
+             os.remove(elt2)
+
+        os.remove('Version')
+        file_list.append('Version')
+        download_file(file_list)
 
 if __name__ == '__main__' :
     if not check_file()[0]:
+        download_file(check_file()[1])
 
-    URL = "https://www.stackoverflow.com/favicon.ico"
-    response = requests.get(URL)
-    if 'file_exist' == False:
-        pass
-    if True :
-        version_control()
+    version_control()
